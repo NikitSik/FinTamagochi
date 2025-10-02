8a267dac36ded439f061ab4b4885bd550195adb9
NikiTOS <97911024+NikitSik@users.noreply.github.com>
2025-10-02 18:56:59 +0400

    улучщил инвентарь

--- Tamagochi/obj/Debug/net9.0/Tamagochi.AssemblyInfo.cs
+++ Tamagochi/obj/Debug/net9.0/Tamagochi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tamagochi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94d812f03134bf165db8cd5bbd03b1c3fdbc2a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c211b5127dbb9481b45207e2428d3c3b0e038148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tamagochi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tamagochi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
